55be4b88426dbd681a0f984e63a1c83a1155b2b0
ksimanov <ksimanov@gmail.com>
2017-08-17 16:42:40 +0600

    #: Add options switcher;

--- src/app/audio-recorder/audio-recorder.directive.js
+++ src/app/audio-recorder/audio-recorder.directive.js
@@ -56,7 +56,23 @@ var LIBVORBIS_DEFAULTS = {
   }
 };
 
-var RECORDER_DEFAULTS = LIBVORBIS_DEFAULTS;
+var WAV_DEFAULTS = {
+  plugins: {
+    record: {
+      audioEngine: 'recorder.js',
+      audioWorkerURL: ''
+    }
+  }
+};
+
+var TYPE_VALUES = ['webm', 'ogg', 'wav', 'mp3'];
+
+var TYPE_MAP = {
+  'mp3': MP3_DEAFAULTS,
+  'webm': WEBM_DEAFULTS,
+  'ogg': LIBVORBIS_DEFAULTS,
+  'wav': WAV_DEFAULTS
+};
 
 function recorder(FileManagerService) {
   return {
@@ -68,19 +84,39 @@ function recorder(FileManagerService) {
     templateUrl: 'app/audio-recorder/audio-recorder.template.html',
 
     link: function ($scope, $element) {
+      $scope.engines = TYPE_VALUES;
+      $scope.currentEngine = TYPE_VALUES[0];
+
       function init() {
         $scope.readyToSave = false;
-        var playerNode = $element[0].querySelector('.video-js');
-        playerNode.id = 'player_' + $scope.$id;
 
-        $scope.options = _.merge($scope.options, DEFAULTS);
+        console.log($scope.options);
+        if (!$scope.options) {
+          $scope.options = _.merge(DEFAULTS, $scope.options);
+        }
+        console.log($scope.options);
 
-        $scope.options.plugins.record.audioEngine = RECORDER_DEFAULTS.plugins.record.audioEngine;
-        $scope.options.plugins.record.audioWorkerURL = RECORDER_DEFAULTS.plugins.record.audioWorkerURL;
+        $scope.options = _.merge($scope.options, TYPE_MAP[$scope.currentEngine]);
+        console.log($scope.options);
 
-        $scope.player = videojs(playerNode.id, $scope.options);
+        var audioNode = createAudioNode();
+        $element[0].querySelector('.video').appendChild(audioNode);
+        $scope.player = videojs(audioNode.id, $scope.options);
+        bindEvents();
       }
       init();
+
+      function createAudioNode() {
+        var node = new Audio();
+        node.id = 'player_' + $scope.$id;
+        node.className = 'video-js vjs-default-skin';
+
+        return node;
+      }
+      function reInit() {
+        $scope.player.dispose();
+        init();
+      }
 
       function bindEvents() {
         $scope.player.on('startRecord', function () {
@@ -97,7 +133,8 @@ function recorder(FileManagerService) {
           console.log('Device error:', $scope.player.deviceErrorCode);
         });
       }
-      bindEvents();
+
+      $scope.setEngine = reInit;
 
       $scope.$on('$destroy', function () {
         $scope.player.dispose();
